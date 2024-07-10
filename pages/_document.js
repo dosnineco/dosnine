6b6306ba743c28aab26d3392d87d3f9963f3f360
dosnineco <122121565+dosnineco@users.noreply.github.com>
2024-07-10 15:53:41 +0000

    out

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+const gtag= 'G-SC64X5RNW0';
 
 class MyDocument extends Document {
 
- 
   render() {
     return (
       <Html lang="en">
           <Head>
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-SC64X5RNW0`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`}
           />           
           <script
             dangerouslySetInnerHTML={{
@@ -17,7 +17,7 @@ class MyDocument extends Document {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-SC64X5RNW0', {
+                gtag('config', ${gtag}, {
                   page_path: window.location.pathname,
                 });
             `,
